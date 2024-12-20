3a294714d577ad6b0d869afb671cfd27d498715a
sophiakohler22 <sophia.kohler@epfl.ch>
2024-12-20 17:14:38 +0100

    Auto-save

--- Jucar_river_basin_APP.py
+++ Jucar_river_basin_APP.py
@@ -28,32 +28,44 @@ st.title("Júcar River Basin - Water management visualization tool")
 if menu == "Home":
     st.header("0. Home page")
     # Create Tabs in Home Section
-    tab1, tab2, tab3 = st.tabs(["Overview", "Key Features", "Instructions"])
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Overview", "Vensim model - View 1", "Vensim model - View 2", "Vensim model - View 3","Vensim model - View 4","Vensim model - View 3","Vensim model - View 5","Vensim model - View 6"])
     
     with tab1:
-        st.subheader("Overview")
+        st.header("Overview")
         st.write("""
         The Júcar River Basin System Dynamics Model is designed to simulate water flows, reservoir levels, and drought conditions. 
         It helps analyze the effects of water management policies on the environment and economy.
         """)
-    
-    with tab2:
-        st.subheader("Key Features")
+        st.header("Key Features")
         st.write("""
         - **Visualize Water Resource Dynamics**: Explore flows between reservoirs, aquifers, and demands.
         - **Interactive Variables**: Adjust reservoir operating rules and test drought measures.
         - **Performance Indicators**: Monitor compliance with ecological flows and economic impacts.
         """)
-    
-    with tab3:
-        st.subheader("Instructions")
+        st.header("Instructions")
         st.write("""
         1. **Navigate** through the app using the menu on the left.
         2. **Explore Key Variables**: Use sliders and inputs to adjust flows, demands, and policies.
         3. **Simulate Scenarios**: Observe results in charts and tables.
         """)
-        
-    
+    with tab2:
+        st.header("Vensim model - View 1  - SYSTEM NETWORK " )
+        st.image("View_1.PNG", caption="General view of the system the Júcar River Model", use_column_width=True)
+    with tab3:
+        st.header("Vensim model - View 2 - MANCHA ORIENTAL AQUIFER " )
+        st.image("View_2.PNG", caption="Subsystem for the stream aquifer interaction between the Jucar River and the Mancha Oriental aquifer", use_column_width=True)
+    with tab4:
+        st.header("Vensim model - View 3 - WATER DEMAND, SUPPLY AND DEFICIT " )
+        st.image("View_3.PNG", caption="The Júcar River, Spain", use_column_width=True)
+    with tab5:
+        st.header("Vensim model - View 4 - RESERVOIRS OPERATING RULES " )
+        st.image("View_4.PNG", caption="The Júcar River, Spain", use_column_width=True)        
+    with tab6:
+        st.header("Vensim model - View 5 - STATE INDEX" )
+        st.image("View_5.PNG", caption="The Júcar River, Spain", use_column_width=True)      
+    with tab7:
+        st.header("Vensim model - View 6 - CROPS" )
+        st.image("Crops.PNG", caption="The Júcar River, Spain", use_column_width=True)    
 
 
 
