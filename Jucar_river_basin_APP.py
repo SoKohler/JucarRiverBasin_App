e64eab7a79f6830cc24db7b9378ca7d4f9c5b3f2
sophiakohler22 <sophia.kohler@epfl.ch>
2024-12-20 19:20:34 +0100

    Auto-save

--- Jucar_river_basin_APP.py
+++ Jucar_river_basin_APP.py
@@ -18,16 +18,16 @@ from streamlit_echarts import st_echarts
 import openpyxl
 from openpyxl import load_workbook
 
-### 1.Read data
-#transform all the formulas into static number 
-workbook = load_workbook('data_formulas.xlsx', data_only=True) 
-sheet = workbook.active #it will read all the sheets 
-#overwrite formulas with values
-for row in sheet.iter_rows():
-    for cell in row:
-        if cell.data_type == 'f':  # if the cell contains a formula
-            cell.value = cell.value  #replace the formula by the static value
-workbook.save(r'C:\Users\sophi\myCloud\Sophia\Thesis\Model\Jucar_model\Adrià\data.xlsx')
+# ### 1.Read data
+# #transform all the formulas into static number 
+# workbook = load_workbook('data_formulas.xlsx', data_only=True) 
+# sheet = workbook.active #it will read all the sheets 
+# #overwrite formulas with values
+# for row in sheet.iter_rows():
+#     for cell in row:
+#         if cell.data_type == 'f':  # if the cell contains a formula
+#             cell.value = cell.value  #replace the formula by the static value
+# workbook.save(r'C:\Users\sophi\myCloud\Sophia\Thesis\Model\Jucar_model\Adrià\data.xlsx')
 
 
 # Import the data with all sheets (type = dictionary)
