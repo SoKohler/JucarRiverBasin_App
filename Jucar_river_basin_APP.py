dfe7f495f113664627512ea36a103fadb3537093
sophiakohler22 <sophia.kohler@epfl.ch>
2024-12-20 17:43:11 +0100

    Auto-save

--- Jucar_river_basin_APP.py
+++ Jucar_river_basin_APP.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from streamlit_echarts import st_echarts
 
 # Page configuration
-st.set_page_config(page_title="Júcar River Basin Water management visualization tool", layout="centered")
+st.set_page_config(page_title="Júcar River Basin Water management visualization tool", layout="centered",initial_sidebar_state="expanded")
 
 ### Dynamic navigation menu 
 menu = st.sidebar.radio( "Menu",
